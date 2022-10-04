781ffedce25fe0a372fadf617938573956b7c9c1
Gabriel Velasquez <114885571+gabovm03@users.noreply.github.com>
2022-10-03 19:39:45 -1200

    Clase 1: Avance

---
+++ dino_runner/components/dinosaurio.py
@@ -0,0 +1,31 @@
+from utils.constants import RUNNING
+from pygame.sprite import Sprite
+class Dinosaur(Sprite):
+    X_POS = 30
+    X_POS = 300
+
+    def __init__(self):
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.X_POS
+        self.dino_run = True
+        self.dino_step = 0
+    
+    def update(self):
+        self.run()
+
+        if self.dino_step >= 10:
+            self.dino_step = 0
+
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+
+    def run(self):
+        self.image = RUNNING[0] if self.dino_step < 5 else RUNNING[1]
+        
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.X_POS
+        self.dino_step +=1
